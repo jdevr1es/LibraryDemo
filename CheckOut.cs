88c035e7305f03ad1482df8e6cb85233b5018420
John DeVries <jdevr1es@users.noreply.github.com>
2021-08-05 15:47:24 -0700

    Add files via upload

---
+++ CheckOut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoApp
+{
+    class CheckOut
+    {
+        public string ItemBorrowedId { get; set; }
+        public string BorrowId { get; set; }
+        public DateTime CheckoutDateTime { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime CheckinDateTime { get; set; }
+        public ItemCondition CheckinCondition { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoApp
+{
+    public abstract class Item
+    {
+        public string ItemId { get; set; }
+        public int ItemType { get; set; }
+        public int NumberOfCopies { get; set; }
+        public CheckedInLocation Location { get; set; }
+
+        //Age below which may not borrow
+        public int AgeRestriction { get; set; }
+        public int ItemCondition { get; set; }
+    }
+
+    public class CheckedInLocation
+    {
+        public string BranchName { get; set; }
+        public string Aisle { get; set; }
+        public string Shelf { get; set; }
+    }
+
+     enum ItemCondition
+    {
+        Good,
+        Fair,
+        Poor,
+        Damaged,
+        Unusable
+    }
+
+    enum ItemType
+    {
+        HardcoverBook,
+        SoftcoverBook,
+        eBook,
+        CD,
+        DVD,
+        Newspaper
+    }
+}
